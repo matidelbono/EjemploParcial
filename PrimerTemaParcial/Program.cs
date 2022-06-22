896305f8ee0a51202adde2a3896bddb8060d46f7
Mati <mati.delbono00@gmail.com>
2022-06-22 10:54:47 -0300

    Primer Ejemplo Funcionante

---
+++ PrimerTemaParcial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using logica;
+using static logica.Delegados;
+
+namespace PrimerTemaParcial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ejemplo ejemplo = new Ejemplo();
+            Numeros delegado1 = new Numeros(ejemplo.TipoNumero);
+            Numeros delegado2 = new Numeros(Ejemplo.SignoNumero);
+            Console.WriteLine("{0 } es {1 }; usar signo\"{ 2 }\".", 5, delegado1(5), delegado2(5));
+            Console.WriteLine("{0 } es {1 }; usar signo\"{ 2 }\".", -3, delegado1(-3), delegado2(-3));
+            Console.WriteLine("{0 } es {1 }; usar signo\"{ 2 }\".", 0, delegado1(0), delegado2(0));
+
+
+        }
+       
+    }
+}
+
+    
---
+++ logica/Delegados.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace logica
+{
+    public class Delegados
+    {
+
+        public delegate String Numeros(int Nro);
+
+        
+            public class Ejemplo
+        {
+          
+
+            public int Nro { get; set; }
+            public String TipoNumero(int Entero)
+            {
+                if (Entero > 0) return "positivo";
+                if (Entero < 0) return "negativo";
+                return "cero";
+            }
+            public static String SignoNumero(int Entero)
+            {
+                if (Entero > 0) return "+";
+                if (Entero < 0) return "-";
+                return "";
+            }
+           
+    }
+    }
+}
